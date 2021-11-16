79124b51535a9a40aae8e1b75825aebbc27cb673
DESKTOP-3MDLI7K\KV <maksym.chepurnykh@nure.ua>
2021-11-16 15:05:47 +0200

    Initial commit

---
+++ lib/chuck.dart
@@ -0,0 +1,8 @@
+class ChuckJoke {
+  String iconUrl;
+  String joke;
+  ChuckJoke(
+      {this.iconUrl =
+          'https://images01.military.com/sites/default/files/styles/full/public/2021-04/chucknorris.jpeg.jpg',
+      this.joke = 'Awesome joke'});
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_network/chuck.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var chuckJoke = ChuckJoke();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image(image: NetworkImage(chuckJoke.iconUrl)),
+            Text(chuckJoke.joke),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var dialogContext;
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  dialogContext = context;
+                  return const Center(
+                    child: SpinKitRing(color: Colors.black),
+                  );
+                });
+            final response = await http
+                .get(Uri.parse('https://api.chucknorris.io/jokes/random'));
+            setState(() {
+              if (response.statusCode == 200) {
+                var jsoneRespose =
+                    jsonDecode(response.body) as Map<String, dynamic>;
+                chuckJoke.joke = jsoneRespose['value'];
+                chuckJoke.iconUrl = jsoneRespose['icon_url'];
+              }
+            });
+            Navigator.pop(dialogContext);
+          },
+        ),
+      ),
+    );
+  }
+}
